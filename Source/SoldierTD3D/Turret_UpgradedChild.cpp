1dd7de652432ad92dc1417fd7349a2500c0e7cee
Mien (Josephine) Nguyen <mien.t.nguyen.22@dartmouth.edu>
2019-08-04 22:25:32 +0700

    Update Turret_UpgradedChild.cpp

--- Source/SoldierTD3D/Turret_UpgradedChild.cpp
+++ Source/SoldierTD3D/Turret_UpgradedChild.cpp
@@ -8,15 +8,14 @@
 
 void ATurret_UpgradedChild::BeginPlay() {
 
-	AActor::BeginPlay(); // So the basic turret's money check doesn't happen; this turret is more expensive
-
+	Super::BeginPlay();
+	
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerPawn::StaticClass(), ExistingPlayerPawn);
 
 	if (ExistingPlayerPawn.Num() > 0) {
 		APlayerPawn* PlayerPawn = Cast<APlayerPawn>(ExistingPlayerPawn[0]);
 
 		if (PlayerPawn) {
-			UE_LOG(LogTemp, Warning, TEXT("About to spend upgrade money"));
 			PlayerPawn->SpendUpgradeTurretMoney();
 		}
 	}
